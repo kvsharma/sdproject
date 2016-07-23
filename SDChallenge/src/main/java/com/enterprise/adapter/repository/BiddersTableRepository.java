d3dd8190aa16f4b770c0707c14a3fd66930d62ab
Karmveer Sharma <karmveer.sharma@Karmveer.local>
2016-07-23 17:27:04 +0530

    merge

--- SDChallenge/src/main/java/com/enterprise/adapter/repository/BiddersTableRepository.java
+++ SDChallenge/src/main/java/com/enterprise/adapter/repository/BiddersTableRepository.java
@@ -1,11 +1,8 @@
 package com.enterprise.adapter.repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.enterprise.adapter.domain.Bidders;
@@ -20,10 +17,5 @@ public interface BiddersTableRepository extends JpaRepository<Bidders, Integer> 
 	List<Bidders> findByProductBidId(Long id);
 
 	List<Bidders> findAll();
-
-	@Query("Select bidders.id as bidderId, max(bidders.amount) from Bidders bidders, Product_Bids productBids "
-			+ "where bidders.productBidId = productBids.id" + " and productBids.bidStartTime<(:currentTime)"
-			+ " and productBids.bidStopTime>(:currentTime)  group by bidderId")
-	List<Bidders> getIntermediateWinners(@Param("currentTime") LocalDateTime currentTime);
 
 }
--- SDChallenge/src/main/java/com/enterprise/adapter/service/impl/ProductBidsTableServiceImpl.java
+++ SDChallenge/src/main/java/com/enterprise/adapter/service/impl/ProductBidsTableServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.enterprise.adapter.domain.ProductBids;
 import com.enterprise.adapter.repository.ProductBidsTableRepository;
@@ -18,6 +19,7 @@ import com.enterprise.adapter.service.ProductBidTableService;
  * @author karmveer.sharma
  *
  */
+@Service
 public class ProductBidsTableServiceImpl implements ProductBidTableService {
 
 	private static final Logger logger = LoggerFactory.getLogger(ProductBidsTableServiceImpl.class);
